4718c88fc15ea8f883b3695dbb4548a40ff2dca2
JamInTheJukebox <44655457+JamInTheJukebox@users.noreply.github.com>
2021-01-27 21:37:18 -0600

    Gigantamax Rat

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,9 +10,25 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     public Healthbar HealthBar_UI;
 
+    #region Big Rat
+    [Header("Big Rat Only")]
+    [Tooltip("Is the rat beeg?")]
+    [SerializeField] bool Gigantamax = false;
+    [SerializeField] GameObject SmallRat;
+    Pathfinding.AIDestinationSetter Target;
+    [SerializeField] int MaxEnemiesToSpawn = 20;
+    [Range(0,3)][SerializeField] float MaxPositionOffset;
+    float totalHealth;          // enemy will shrink based on their health
+    #endregion
+
     private void Awake()
     {
         HealthBar_UI.InitializeHealth(health);
+        if (Gigantamax)
+        {
+            Target = GetComponent<Pathfinding.AIDestinationSetter>();
+            totalHealth = health;
+        }
     }
 
 
@@ -20,10 +36,33 @@ public class EnemyController : MonoBehaviour
     {
         health -= damage;
         HealthBar_UI.UpdateHealth_UI(health);
+
+        if (Gigantamax)         // big rats spawning smaller rats
+        {
+            HandleLargeEnemy();
+        }
+
         if (health <= 0) 
         {
             ScreenShakeController.instance.ShakeScreen(0.1f, 0.1f);
             Destroy(this.gameObject);
+        }
+    }
+
+    private void HandleLargeEnemy()
+    {
+        float newScale = 1 + health / totalHealth;
+        transform.localScale = new Vector3(newScale, newScale, 1);          // The rat will shrink as it is shot
+
+        if (Target.target != null && MaxEnemiesToSpawn > 0)
+        {
+            var newEnemy = Instantiate(SmallRat, transform.position, Quaternion.identity);
+            float _XOffset = Random.Range(-MaxPositionOffset, MaxPositionOffset);
+            float _YOffset = Random.Range(-MaxPositionOffset, MaxPositionOffset);
+            newEnemy.transform.position += new Vector3(_XOffset, _YOffset, 0);
+            MaxEnemiesToSpawn--;
+            newEnemy.GetComponent<Pathfinding.AIDestinationSetter>().target = Target.target;
+            
         }
     }
 }
