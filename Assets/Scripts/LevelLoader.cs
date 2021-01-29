8c7c26c76af21e1156a329189de9ed1ad35616e2
JonathanKonderla <JonathanKonderla@gmail.com>
2021-01-28 22:16:27 -0600

    Main Menu

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        
+        if(nextIndex >= SceneManager.sceneCountInBuildSettings){
+            return;
+        }
+
+        StartCoroutine(LoadLevel(nextIndex));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+        
+        SceneManager.LoadScene(levelIndex);
+    }
+}
